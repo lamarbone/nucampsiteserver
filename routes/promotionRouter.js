bf334c477c4e17acf8f9080ef5d5fce59bc7a710
Bryan <blcolley@me.com>
2022-11-06 19:58:04 -0500

    express generator 2

---
+++ routes/promotionRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const promotionRouter = express.Router();
+
+promotionRouter.route('/')
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+
+.post((req, res) => {
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+})
+
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+
+.delete((req, res) => {
+    res.end('Deleting all promotions')
+});
+
+
+promotionRouter.route('/:promotionId')
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end(`Will send details of the promotions: ${req.params.promotionId}`);
+})
+
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions');
+})
+
+.put((req, res) => {
+    res.end(`Updating promotions with new name: ${req.body.name} 
+    and description: ${req.body.description}`);
+})
+
+.delete((req, res) => {
+    res.end(`Deleting a promotion with id: ${req.params.promotionId}`)
+});
+
+
+module.exports = promotionRouter;
